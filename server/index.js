4262d99e8bbc7e805244e661079854bdd5129c3f
WalterT-MK <tangtgs1516993@gmail.com>
2021-10-06 11:59:38 -0700

    Loader.io verify

--- server/index.js
+++ server/index.js
@@ -21,6 +21,12 @@ app.get('/reviews/meta', db.getMeta);
 app.put('/reviews/:review_id/helpful', db.setHelpful);
 app.put('/reviews/:review_id/report', db.setReport);
 
+
+//loader.io
+app.get('/loaderio-94a7fda4dcb555e4f2905fbcf0853aff', (req,res) => {
+  res.status(200).send('loaderio-94a7fda4dcb555e4f2905fbcf0853aff');
+})
+
 app.listen(port, () => {
   console.log(`listening to port: ${port}`);
 })
